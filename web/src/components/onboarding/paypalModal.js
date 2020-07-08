553126b72b312365a9623b85df52b147516d7351
orkun mert <orkunmertgs41@gmail.com>
2020-07-08 19:59:40 +0300

    socialmedia , paypal to onboarding

--- web/src/components/onboarding/paypalModal.js
+++ web/src/components/onboarding/paypalModal.js
@@ -4,6 +4,7 @@ import { PayPalButton } from 'react-paypal-button-v2'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import paypalConfig from '../../config/paypal.json'
+
 import { downgrade, upgrade } from '../../store/actions/authActions'
 import Modal from '../modal/Modal'
 
@@ -13,58 +14,22 @@ class UpgradeProPremium extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			isProPremium: false
+            isProPremium: false,
+            openModal:false,
+            
 		}
 		this.handleSubmit = this.handleSubmit.bind(this)
-		this.cancelSubscription = this.cancelSubscription.bind(this)
+		
 	}
 
-	cancelSubscription = () => {
-		let paypalBaseUri = 'https://api.paypal.com/';
-		if (paypalConfig.sandbox) paypalBaseUri = 'https://api.sandbox.paypal.com/';
+    handleSelector=(e)=>{
 
-		console.log('btn clicked')
-		let $this = this
-		axios.post(`${paypalBaseUri}v1/oauth2/token`,"grant_type=client_credentials",{
-			headers:{
-				"Accept" : "application/json",
-	   	  		"Accept-Language" : "en_US",
-	   	  		'Access-Control-Allow-Origin': 'localhost:3000'
-			},
-			auth: {
-				username: paypalConfig.client_id,
-				password: paypalConfig.client_secret
-			}
-		}).then(response=>{
-			let paypal_access_token = response.data.access_token;
-	   		let paypal_token_type = response.data.token_type;
+this.setState({openModal:e.target.checked})
+    }
 
-			axios({
-				url: `${paypalBaseUri}v1/billing/subscriptions/${this.props.profile.paypalPremium.id}/cancel`,
-				method: 'post',
-				headers: {
-					"Content-Type": "application/json",
-					"Authorization":`${paypal_token_type} ${paypal_access_token}`
-				},
-				data: { "reason": "test -- Not satisfied with the service" }
-			})
-				.then(res => {
-					console.log(`Axios Call completed: ${res}`)
-					this.setState({
-						isProPremium: false,
-					})
-					setTimeout(function () {
-						// console.log('please', $this.props, $this.state);
-						$this.props.downgrade($this.state)
-						document.body.style.overflow = 'unset'
-						$this.props.history.push('/profile-edit')
-					}, 3000)
-				});
-		})
-	}
 
 	handleSubmit = (details, data) => {
-		console.log("nnn",details)
+        console.log("nnn",details)
 		let $this = this
 		this.setState({
 			isProPremium: true,
@@ -90,8 +55,9 @@ class UpgradeProPremium extends Component {
 		setTimeout(function () {
 			// console.log('please', $this.props, $this.state);
 			$this.props.upgrade($this.state)
-			document.body.style.overflow = 'unset'
-			$this.props.history.push('/profile-edit')
+            document.body.style.overflow = 'unset'
+            
+			
 		}, 3000)
 	}
 
@@ -100,25 +66,20 @@ class UpgradeProPremium extends Component {
 		return (
 			<div className={'pro-premium'}>
 				<h3>Pro Premium</h3>
-				<p>You are currently <strong>{this.props.profile.isProPremium ? 'enrolled' : 'not enrolled'}</strong> in the Pro Premium program.</p>
+			
 				{this.props.profile.isProPremium ?
-					<Modal
-						buttonText={'Downgrade'}
-						buttonStyle={'button button--md button--secondary'}
+				<h1>You are premium user !</h1>
+                    :
+                    <div>
+                   
+                
+                 <Modal
+						buttonText={'Be Premium'}
+                        buttonStyle={'button button--md button--secondary'}
+                        
 						content={(
 							<>
-								<h2>Confirm Downgrade</h2>
-								<button onClick={this.cancelSubscription}>Downgrade</button>
-							</>
-						)}
-					/>
-					:
-					<Modal
-						buttonText={'Upgrade'}
-						buttonStyle={'button button--md button--secondary'}
-						content={(
-							<>
-								<h2>Confirm Upgrade</h2>
+								<h2>be Premium</h2>
 								<PayPalButton
 									options={{
 										clientId: paypalConfig.client_id,
@@ -140,7 +101,7 @@ class UpgradeProPremium extends Component {
 								/>
 							</>
 						)}
-					/>
+                                /></div>
 				}
 			</div>
 		);
@@ -161,7 +122,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
 	return {
 		upgrade: (upgradeParams) => dispatch(upgrade(upgradeParams)),
-		downgrade: (downgradeParams) => dispatch(downgrade(downgradeParams))
+		
 	}
 }
 
